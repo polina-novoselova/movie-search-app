75be7587a0d347bedfa1ce71b81efaf76ba4989a
Polina Novoselova <polka.nov@mail.ru>
2023-08-30 13:57:10 +0200

    add api key and done search film by name

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const inputMovieAdderNode = document.getElementById("inputMovieAdder");
 const btnAddMovieNode = document.getElementById("btnAddMovie");
-const moviesListNode = document.getElementById("movie-items");
+const moviesNode = document.getElementById("movie-items");
 
-let moviesList = [];
+let movies = [];
 
 // init();
 
-function addMovie() {
-  const filmName = getMovieNameFromUser();
-  const movieId = moviesList.length + 1;
+const getMovie = async (filmName) => {
+  
+  try {
+    const response = await fetch(`http://www.omdbapi.com/?t=${filmName}&apikey=6152c476`);
 
-  if (!validate(filmName)) {
-    alert("Пожалуйста, введите корректное название фильма (не более 50 символов).");
-    return;
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    setMovie(data.Title, data.imdbID, data.Year, data.Country, data.Genre, data.Director, data.Type, data.imdbRating, data.Poster);
+    console.log(data);
+    renderMovies();
+    return data;
+
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 
-  setMovie(filmName, movieId);
-  saveMoviesToStorage();
-  clearInput();
-  renderMovies();
+};
+
+async function addMovie() {
+  const filmName = getMovieNameFromUser();
+
+  if (filmName.trim() !== "") {
+    try {
+      await getMovie(filmName);
+      clearInput();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };
 
 function getMovieNameFromUser() {
@@ -26,72 +47,106 @@ function getMovieNameFromUser() {
   return filmName;
 };
 
-function setMovie(filmName, movieId) {
-  moviesList.push({
-    name: filmName,
-    id: movieId,
-    checked: false,
+function setMovie(Title, omdbId, Year, Country, Genre, Director, Type, imdbRating, poster) {
+  movies.push({
+    name: Title,
+    id: omdbId,
+    year: Year,
+    country: Country,
+    genre: Genre,
+    director: Director,
+    type: Type,
+    rating: imdbRating,
+    poster: poster,
   });
 };
 
-function saveMoviesToStorage() {
-  localStorage.setItem("movies", JSON.stringify(moviesList));
-};
+// function saveMoviesToStorage() {
+//   localStorage.setItem("movies", JSON.stringify(movies));
+// };
 
-function getMoviesFromStorage() {
-  const savedMovies = localStorage.getItem("movies");
-  if (savedMovies) {
-    moviesList = JSON.parse(savedMovies);
-  }
-};
+// function getMoviesFromStorage() {
+//   const savedMovies = localStorage.getItem("movies");
+//   if (savedMovies) {
+//     movies = JSON.parse(savedMovies);
+//   }
+// };
 
 function clearInput() {
   inputMovieAdderNode.value = "";
 };
 
 function renderMovies() {
-  moviesListNode.innerHTML = "";
-  moviesList.forEach((movie) => {
+  moviesNode.innerHTML = "";
+  movies.forEach((movie) => {
     const movieItem = document.createElement("li");
     movieItem.className = "movie-item";
     movieItem.setAttribute("id", movie.id);
 
-    const movieCheckbox = document.createElement("input");
-    movieCheckbox.className = "checkbox";
-    movieCheckbox.setAttribute("type", "checkbox");
-    movieCheckbox.setAttribute("checked", "false");
-    movieCheckbox.checked = movie.checked;
+    const poster = document.createElement("img");
+    poster.className = "poster";
+    poster.setAttribute("src", movie.poster);
+    poster.setAttribute("alt", "movie poster");
 
-    const fakecheckbox = document.createElement("span");
-    fakecheckbox.className = "fakecheckbox";
-    fakecheckbox.checked = movie.checked;
+    const infoBlock = document.createElement("div");
+    infoBlock.className = "info-block";
 
-    const movieLabel = document.createElement("label");
-    movieLabel.className = "label";
+    const movieInfo = document.createElement("div");
+    movieInfo.className = "movie-info";
 
-    const movieName = document.createElement("span");
+    const movieName = document.createElement("h2");
     movieName.className = "movie-name";
     movieName.innerHTML = movie.name;
 
-    const deleteMovieBtn = document.createElement("button");
-    deleteMovieBtn.className = "btn-delete-item";
-    deleteMovieBtn.setAttribute("id", movie.id);
-    deleteMovieBtn.setAttribute("title", "Удалить");
+    const year = document.createElement("p");
+    year.className = "year";
+    year.innerHTML = movie.year;
 
-    movieLabel.appendChild(movieCheckbox);
-    movieItem.appendChild(movieLabel);
-    movieLabel.appendChild(fakecheckbox); 
-    movieLabel.appendChild(movieName);
-    movieItem.appendChild(deleteMovieBtn);
+    const about = document.createElement("p");
+    about.className = "about";
+    about.innerHTML = `${movie.country} • ${movie.genre}`;
 
-    moviesListNode.appendChild(movieItem);
+    const director = document.createElement("p");
+    director.className = "director";
+    director.innerHTML = `Director: ${movie.director}`;
+
+    const type = document.createElement("p");
+    type.className = "type";
+    type.innerHTML = `Category: ${movie.type}`;
+    
+
+    const secondBlock = document.createElement("div");
+    secondBlock.className = "second-block";
+
+    const deleteItemBtn = document.createElement("button");
+    deleteItemBtn.className = "btn-delete-item";
+    deleteItemBtn.setAttribute("id", movie.id);
+    deleteItemBtn.setAttribute("title", "Delete");
+
+    const rating = document.createElement("p");
+    rating.className = "rating";
+    rating.innerHTML = `IMDb: ${movie.rating}`;
+
+    movieItem.appendChild(poster);
+    movieItem.appendChild(infoBlock);
+    infoBlock.appendChild(movieInfo);
+    movieInfo.appendChild(movieName); 
+    movieInfo.appendChild(year);
+    movieInfo.appendChild(about);
+    movieInfo.appendChild(director);
+    movieInfo.appendChild(type);
+    movieItem.appendChild(secondBlock); 
+    secondBlock.appendChild(deleteItemBtn);
+    secondBlock.appendChild(rating);    
+
+    moviesNode.appendChild(movieItem);
   });
 };
 
-function init() {
-  getMoviesFromStorage();
-  renderMovies();
-};
+// function init() {
+//   getMoviesFromStorage();
+//   renderMovies();
+// };
 
 
 function deleteMovie(event) {
@@ -100,62 +155,46 @@ function deleteMovie(event) {
     const parentNode = event.target.closest(".movie-item");
     const idParentNode = Number(parentNode.id);
 
-    const index = moviesList.findIndex(function (movie) {
+    const index = movies.findIndex(function (movie) {
       if (movie.id === idParentNode) {
         return true;
       }
     });
 
-    moviesList.splice(index, 1);
+    movies.splice(index, 1);
     parentNode.remove();
-    saveMoviesToStorage();
+    // saveMoviesToStorage();
   }
 };
 
-function checkedMovie(event) {
-  if (event.target.classList.contains("label")) {
-    const parentNode = event.target.closest(".movie-item");
-    const idParentNode = Number(parentNode.id);
-    const index = moviesList.findIndex(function (movie) {
-      if (movie.id === idParentNode) {
-        return true;
-      }
-    });
+// function validate(filmName) {
+//   if (!filmName) {
+//     return false;
+//   }
 
-    if (moviesList[index].checked === true) {
-      moviesList[index].checked = false;
-    } else {
-      moviesList[index].checked = true;
-    }
-    
-    saveMoviesToStorage();
-  }
-};
+//   if (filmName.length > 50) {
+//     return false;
+//   }
 
-function validate(filmName) {
-  if (!filmName) {
-    return false;
-  }
+//   if (!filmName.trim()) {
+//     return false;
+//   }
 
-  if (filmName.length > 50) {
-    return false;
-  }
+//   return true;
+// }
 
-  if (!filmName.trim()) {
-    return false;
-  }
+// function handlerKeyDown(event) {
 
-  return true;
-}
+//   if (event.key == "Enter") {
+//     addMovie();
+//   }
+// }
 
-function handlerKeyDown(event) {
-
-  if (event.key == "Enter") {
-    addMovie();
-  }
-}
+// function reedMore(event) {
+//  открытие попапа с инфой о фильме
+// };
 
 btnAddMovieNode.addEventListener("click", addMovie);
-moviesListNode.addEventListener("click", deleteMovie);
-moviesListNode.addEventListener("click", checkedMovie);
-inputMovieAdderNode.addEventListener("keydown", handlerKeyDown);
+moviesNode.addEventListener("click", deleteMovie);
+// moviesNode.addEventListener("click", reedMore);
+// inputMovieAdderNode.addEventListener("keydown", handlerKeyDown);
